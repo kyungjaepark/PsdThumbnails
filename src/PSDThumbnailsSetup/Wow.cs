649670f8f6ea93b10196527d6a1a867b5aead613
Kyungjae Park <kyungjae.park@gmail.com>
2018-12-12 01:38:01 +0900

    .NET 3.5 에서 x64 판별을 위한 코드 추가

---
+++ src/PSDThumbnailsSetup/Wow.cs
@@ -0,0 +1,49 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PSDThumbnailsSetup
+{
+    // https://social.msdn.microsoft.com/Forums/vstudio/en-US/24792cdc-2d8e-454b-9c68-31a19892ca53/how-to-check-whether-the-system-is-32-bit-or-64-bit-
+    public static class Wow
+    {
+        public static bool Is64BitProcess
+        {
+            get { return IntPtr.Size == 8; }
+        }
+
+        public static bool Is64BitOperatingSystem
+        {
+            get
+            {
+                // Clearly if this is a 64-bit process we must be on a 64-bit OS.
+                if (Is64BitProcess)
+                    return true;
+                // Ok, so we are a 32-bit process, but is the OS 64-bit?
+                // If we are running under Wow64 than the OS is 64-bit.
+                bool isWow64;
+                return ModuleContainsFunction("kernel32.dll", "IsWow64Process") && IsWow64Process(GetCurrentProcess(), out isWow64) && isWow64;
+            }
+        }
+
+        static bool ModuleContainsFunction(string moduleName, string methodName)
+        {
+            IntPtr hModule = GetModuleHandle(moduleName);
+            if (hModule != IntPtr.Zero)
+                return GetProcAddress(hModule, methodName) != IntPtr.Zero;
+            return false;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        extern static bool IsWow64Process(IntPtr hProcess, [MarshalAs(UnmanagedType.Bool)] out bool isWow64);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        extern static IntPtr GetCurrentProcess();
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        extern static IntPtr GetModuleHandle(string moduleName);
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true)]
+        extern static IntPtr GetProcAddress(IntPtr hModule, string methodName);
+    }
+}
